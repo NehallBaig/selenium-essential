f118821e00ebf3a25d428606f23394631993ad2f
NehallBaig <nehallbaig@gmail.com>
2024-03-03 16:55:58 +0500

    Testing multiproject

---
+++ SeleniumExample/src/test/java/Test.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+    }
+}
